818798a9156f63f0d1861c7abe9903ee28b40a58
nati-tewolde <natisnewemail@gmail.com>
2025-09-24 13:52:43 -0400

    completed step 1 of the exercise

--- src/main/java/com/pluralsight/VariableApp.java
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -1,4 +1,24 @@
 package com.pluralsight;
 
 public class VariableApp {
+    public static void main(String[] args) {
+        String favoriteColor = "Black";
+        String classYear = "2025";
+        char middleInitial = 'T';
+        boolean hasPets = true;
+        String niceMessage = "You look nice today!";
+
+        System.out.println("My favorite color is " + favoriteColor + ".");
+        System.out.println("I started this class during the year " + classYear + ".");
+        System.out.println("My middle initial is " + middleInitial + ".");
+        System.out.println("It would be " + hasPets + " in saying that I have pets.");
+        System.out.println("I head something nice today, it was: " + niceMessage);
+
+        System.out.println("=======================================");
+
+
+
+
+
+    }
 }
